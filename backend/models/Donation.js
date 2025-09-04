b1fa73b4dae90ce56eaa15d6f48bf0e4b510f57f
waqasahmed-01 <waqassoomroa@gmail.com>
2025-09-04 09:19:37 +0500

    New Donation model

---
+++ backend/models/Donation.js
@@ -0,0 +1,65 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+
+//Donation Schema.
+const donationSchema = new mongoose.Schema({
+  foodType: {
+    type: String,
+    enum: ['cooked', 'raw', 'packaged'],
+    required: true,
+  },
+  quantity: {
+    type: String, // Example: "5 kg", "10 meals"
+    required: true,
+    trim: true,
+  },
+  location: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted', 'picked-up', 'completed', 'rejected'],
+    default: 'pending',
+  },
+  donor: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  ngo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // NGO will also be from User collection
+    default: null,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+//Joi Validation.
+function validateDonation(donation) {
+  const schema = Joi.object({
+    foodType: Joi.string().valid('cooked', 'raw', 'packaged').required(),
+    quantity: Joi.string().min(1).max(50).required(),
+    location: Joi.string().min(3).max(255).required(),
+    status: Joi.string().valid(
+      'pending',
+      'accepted',
+      'picked-up',
+      'completed',
+      'rejected'
+    ),
+    donor: Joi.objectId().required(),
+    ngo: Joi.objectId().allow(null),
+  });
+
+  return schema.validate(donation);
+}
+
+const Donation = mongoose.model('Donation', donationSchema);
+
+module.exports = { Donation, validateDonation };
